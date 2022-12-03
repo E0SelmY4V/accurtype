85be375746e1d1b04e4db54f25318f1869099be9
E0SelmY4V-PC <2507917746@qq.com>
2022-12-03 22:03:52 +0800

    ref: Spaceless 改为基于 Replaced

--- lib/sprec.d.ts
+++ lib/sprec.d.ts
@@ -1,5 +1,5 @@
 import { Tostrable, WideNum } from './base'
-type Spaceless<N extends string> = N extends `${infer H} ${infer K}` ? `${H}${Spaceless<K>}` : N
+type Spaceless<N extends string> = Replaced<N, ' ', ''>;
 type Leading0less<N extends string> = N extends `0${infer K}` ? K extends '' ? N : Leading0less<K> : N
 type RevString<N extends string> = N extends `${infer K}${infer S}` ? `${RevString<S>}${K}` : N
 type Replaced<N extends string, F extends Tostrable, T extends Tostrable> = N extends `${infer Q}${F}${infer H}` ? `${Q}${T}${Replaced<H, F, T>}` : N
