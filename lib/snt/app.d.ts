a2e0bc3aaaa34ce981063460b44e9cefc6ac0dab
E0SelmY4V-PC <2507917746@qq.com>
2022-12-04 02:35:28 +0800

    fix: 类型不匹配

--- lib/snt/app.d.ts
+++ lib/snt/app.d.ts
@@ -11,7 +11,7 @@ declare function strlen<N extends string>(str: N): LenOfStr<N>
 type LenOfArr<N extends any[], L extends number = 0> = N extends [] ? L : N extends [any, ...infer S] ? LenOfArr<S, NumOfStr<SntXcrUns<0, `${L}`>, number>> : N extends any[] ? number : L
 declare function arrlen<N extends ArrayAccur>(arr: N): LenOfArr<N>
 
-type Leading0Filled<N extends Tostrable, B extends number | `${number}`> = `${B}` extends '0' ? N : `0${Leading0Filled<N, SntXcrUns<9, `${B}`>>}`
-declare function fillLeading0<N extends Tostrable, B extends number | `${number}`>(num: N, n0: B): Leading0Filled<N, B>
+type Leading0Filled<N extends Tostrable, B extends number | string> = `${B}` extends '0' ? N : `0${Leading0Filled<N, SntXcrUns<9, `${B}`>>}`
+declare function fillLeading0<N extends Tostrable, B extends number | string>(num: N, n0: B): Leading0Filled<N, B>
 
 // type Filled0toSame<N extends string, B extends string, I extends string = '0'> = N[NumOfStr<I, number>] extends void ? 1 : 0
