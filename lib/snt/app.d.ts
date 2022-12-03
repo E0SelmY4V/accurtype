afb5f9059ca8b8e63f550d7872f3e66f29101939
E0SelmY4V-PC <2507917746@qq.com>
2022-12-04 02:18:26 +0800

    add: 增加了函数

--- lib/snt/app.d.ts
+++ lib/snt/app.d.ts
@@ -2,12 +2,16 @@ import {
 	NumOfStr,
 	SntXcrUns,
 	Tostrable,
+	ArrayAccur,
 } from '../..'
-type LenOfStr<N extends string, C extends boolean = false, L extends number = 0> = C extends false
-	? string extends N ? number : LenOfStr<N, true>
-	: N extends `${any}${infer K}` ? LenOfStr<K, true, NumOfStr<SntXcrUns<0, `${L}`>, number>> : L
-type LenOfArr<N extends any[], C extends boolean = false, L extends number = 0> = N extends never[] ? 0 : C extends false
-	? LenOfArr<N, true> extends infer K ? K extends 0 ? N extends [] ? 0 : number : K : 0
-	: N extends [any, ...infer S] ? LenOfArr<S, true, NumOfStr<SntXcrUns<0, `${L}`>, number>> : L
-type Leading0Filled<N extends Tostrable, B extends Tostrable> = `${B}` extends '0' ? N : `0${Leading0Filled<N, SntXcrUns<9, `${B}`>>}`
-type Filled0toSame<N extends string, B extends string, I extends string = '0'> = N[NumOfStr<I, number>] extends void ? 1 : 0
+
+type LenOfStr<N extends string, L extends number = 0> = N extends '' ? L : N extends `${any}${infer K}` ? LenOfStr<K, NumOfStr<SntXcrUns<0, `${L}`>, number>> : N extends string ? number : L
+declare function strlen<N extends string>(str: N): LenOfStr<N>
+
+type LenOfArr<N extends any[], L extends number = 0> = N extends [] ? L : N extends [any, ...infer S] ? LenOfArr<S, NumOfStr<SntXcrUns<0, `${L}`>, number>> : N extends any[] ? number : L
+declare function arrlen<N extends ArrayAccur>(arr: N): LenOfArr<N>
+
+type Leading0Filled<N extends Tostrable, B extends number | `${number}`> = `${B}` extends '0' ? N : `0${Leading0Filled<N, SntXcrUns<9, `${B}`>>}`
+declare function fillLeading0<N extends Tostrable, B extends number | `${number}`>(num: N, n0: B): Leading0Filled<N, B>
+
+// type Filled0toSame<N extends string, B extends string, I extends string = '0'> = N[NumOfStr<I, number>] extends void ? 1 : 0
