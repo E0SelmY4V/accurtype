b0563bb9f37982412dafbaee0c29bf416bd6f5f6
E0SelmY4V-PC <2507917746@qq.com>
2022-12-03 22:02:04 +0800

    ref: 修改导出

--- lib/sprec.d.ts
+++ lib/sprec.d.ts
@@ -1,6 +1,5 @@
 import { Tostrable, WideNum } from './base'
-export { }
-export type Spaceless<N extends string> = N extends `${infer H} ${infer K}` ? `${H}${Spaceless<K>}` : N
-export type Leading0less<N extends string> = N extends `0${infer K}` ? K extends '' ? N : Leading0less<K> : N
-export type RevString<N extends string> = N extends `${infer K}${infer S}` ? `${RevString<S>}${K}` : N
-export type Replaced<N extends string, F extends Tostrable, T extends Tostrable> = N extends `${infer Q}${F}${infer H}` ? `${Q}${T}${Replaced<H, F, T>}` : N
+type Spaceless<N extends string> = N extends `${infer H} ${infer K}` ? `${H}${Spaceless<K>}` : N
+type Leading0less<N extends string> = N extends `0${infer K}` ? K extends '' ? N : Leading0less<K> : N
+type RevString<N extends string> = N extends `${infer K}${infer S}` ? `${RevString<S>}${K}` : N
+type Replaced<N extends string, F extends Tostrable, T extends Tostrable> = N extends `${infer Q}${F}${infer H}` ? `${Q}${T}${Replaced<H, F, T>}` : N
