3dfc8ff8da15f7e01814cdec62dd9a5e32fecc31
E0SelmY4V-PC <2507917746@qq.com>
2022-12-10 20:42:11 +0800

    ref: 导出

--- lib/base.d.ts
+++ lib/base.d.ts
@@ -1,6 +1,8 @@
-export { }
-export type SigNumber = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
-export type WideNum = number | bigint
-export type ArrayAccur = [any, ...any[]] | any[]
-export type Tostrable = string | number | WideNum
-export type NumOfStr<N extends string, T extends Tostrable = WideNum> = N extends `${infer K extends T}` ? K : T
+import {
+} from '..'
+
+type SigNumber = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+type WideNum = number | bigint
+type ArrayAccur = [any, ...any[]] | any[]
+type Tostrable = string | number | WideNum
+type NumOfStr<N extends string, T extends Tostrable = WideNum> = N extends `${infer K extends T}` ? K : T
