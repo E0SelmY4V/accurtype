22da88f54b885ef4aabf46cda6d93c6194239fb5
E0SelmY4V-PC <2507917746@qq.com>
2022-12-11 02:07:15 +0800

    add: 模糊Wide与确切Ltd类型

--- lib/base.d.ts
+++ lib/base.d.ts
@@ -2,7 +2,21 @@ import {
 } from '..'
 
 type SigNumber = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+
+type Not<N extends boolean> = N extends true ? false : true
+
 type WideNum = number | bigint
+type IsWideWideNum<N> = N extends WideNum ? number extends N ? true : bigint extends N ? true : false : false
+type IsLtdWideNum<N> = N extends WideNum ? Not<IsWideWideNum<N>> : false
+
+type IsWideBoolean<N> = TypeOf<N> extends 'boolean' ? boolean extends N ? true : false : false
+type IsLtdBoolean<N> = TypeOf<N> extends 'boolean' ? boolean extends N ? false : true : false
+
+type IsWideString<N> = N extends string ? string extends N ? true : N extends `${infer N0}${infer N1}` ? (
+	N0 extends `${infer H extends WideNum}` ? IsWideWideNum<H> extends true ? true : IsWideString<N1> :
+	string extends N0 ? true : IsWideString<N1>
+) : false : false
+type IsLtdString<N> = N extends string ? Not<IsWideString<N>> : false
 
 interface TypeMapJS {
 	bigint: bigint,
@@ -14,21 +28,19 @@ interface TypeMapJS {
 	symbol: Symbol,
 	undefined: undefined,
 }
+type TypeNameJS = keyof TypeMapJS
+type IsTypeNameJS<N> = IsLtdString<N> extends true ? N extends keyof TypeMapJS ? true : false : boolean
+declare function isTypeNameJS<N, T extends string>(n: N | T): IsTypeNameJS<N>
 declare const TYPE_MAP: TypeMapJS
 declare function str2type<N extends TypeNameJS>(str: N): TypeMapJS[N]
-type TypeNameJS = keyof TypeMapJS
-type IsTypeNameJS<N> = N extends keyof TypeMapJS ? true : false
-declare function isTypeNameJS(n: keyof TypeMapJS): true
-declare function isTypeNameJS(n: any): boolean
 
 interface TypeMap extends TypeMapJS {
 	never: never,
 	void: void,
 }
 type TypeName = keyof TypeMap
-type IsTypeName<N> = N extends keyof TypeMap ? true : false
-declare function isTypeName(n: keyof TypeMap): true
-declare function isTypeName(n: any): boolean
+type IsTypeName<N> = IsLtdString<N> extends true ? N extends keyof TypeMap ? true : false : boolean
+declare function isTypeName<N, T extends string>(n: N | T): IsTypeName<N>
 
 type TypeOf<N> =
 	N extends bigint ? 'bigint' :
@@ -45,5 +57,17 @@ type TypeOf<N> =
 type NameOfType<N> = TypeOf<N>
 
 type ArrayAccur = [any, ...any[]] | any[]
+
+type IsWideArray<N> = N extends (infer T)[] ? T[] extends N ? true : false : false
+type IsLongArray<N> = N extends any[] ? IsWideArray<N> extends true ? true : N extends [any, ...infer K] ? IsLongArray<K> : N extends [] ? false : true : false
+
 type Tostrable = string | boolean | WideNum | null | undefined
+type TostrableType = TypeOf<Tostrable>
+type IsWideTostrable<N> = TypeOf<N> extends TostrableType ? (
+	IsWideBoolean<N> extends true ? true :
+	IsWideWideNum<N> extends true ? true :
+	IsWideString<N>
+): false
+type IsLtdTostrable<N> = TypeOf<N> extends TostrableType ? Not<IsWideTostrable<N>> : false
+
 type NumOfStr<N extends string, T extends Tostrable = WideNum> = N extends `${infer K extends T}` ? K : T
