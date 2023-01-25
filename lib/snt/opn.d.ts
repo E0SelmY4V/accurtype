3070b689dc2839a8916b2da2e7bc7cb59ad54b06
E0SelmY4V-PC <2507917746@qq.com>
2023-01-25 17:00:17 +0800

    Ref snt模块化改造

--- lib/snt/opn.d.ts
+++ lib/snt/opn.d.ts
@@ -4,13 +4,13 @@ import {
 	WideNum,
 } from '..'
 import {
-	SntAosHal,
-	SntR,
+	opn,
+	xcr,
 } from './tool'
 
-type SntAosUns<T extends 0 | 9, A extends string, B extends string> = `${T},${SntCmpUns<A, B>}` extends '9,1' ? `-${SntAosHal<T, B, A>}` : SntAosHal<T, A, B>
+type SntAosUns<T extends 0 | 9, A extends string, B extends string> = `${T},${SntCmpUns<A, B>}` extends '9,1' ? `-${opn.AosHal<T, B, A>}` : opn.AosHal<T, A, B>
 
-type SntAos<T extends 0 | 9, A extends string, B extends string> = A extends `-${infer A}` ? B extends `-${infer B}` ? `${T extends 0 ? '-' : ''}${SntAosUns<T, B, A>}` : `${T extends 9 ? '-' : ''}${SntAosUns<SntR<T>, B, A>}` : B extends `-${infer B}` ? SntAosUns<SntR<T>, A, B> : SntAosUns<T, A, B>
+type SntAos<T extends 0 | 9, A extends string, B extends string> = A extends `-${infer A}` ? B extends `-${infer B}` ? `${T extends 0 ? '-' : ''}${SntAosUns<T, B, A>}` : `${T extends 9 ? '-' : ''}${SntAosUns<xcr.R<T>, B, A>}` : B extends `-${infer B}` ? SntAosUns<xcr.R<T>, A, B> : SntAosUns<T, A, B>
 
 type SntAosNum<T extends 0 | 9, A extends WideNum, B extends WideNum, K extends WideNum> = NumOfStr<SntAos<T, `${A}`, `${B}`>, K>
 
--- lib/snt/xcr.d.ts
+++ lib/snt/xcr.d.ts
@@ -5,14 +5,13 @@ import {
 	Replaced,
 } from '../..'
 import {
-	SntXcrOri,
-	SntR,
+	xcr,
 } from './tool'
 
-type SntXcrUns<T extends 0 | 9, N extends string> = Leading0less<Replaced<SntXcrOri<T, `0${N}`>, 's', T>>
+type SntXcrUns<T extends 0 | 9, N extends string> = Leading0less<Replaced<xcr.Ori<T, `0${N}`>, 's', T>>
 
 type SntXcr<T extends 0 | 9, N extends string> = `${T},${N}` extends '0,-1' ? '0' : `${T},${N}` extends '9,0' ? '-1' :
-	N extends `-${infer K}` ? `-${SntXcrUns<SntR<T>, K>}` : SntXcrUns<T, N>
+	N extends `-${infer K}` ? `-${SntXcrUns<xcr.R<T>, K>}` : SntXcrUns<T, N>
 
 type SntXcrNum<T extends 0 | 9, N extends WideNum, K extends WideNum> = NumOfStr<SntXcr<T, `${N}`>, K>
 
