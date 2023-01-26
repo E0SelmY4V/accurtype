3240b9bdd34cefa4f4972a53c33cf5d921dc2cea
E0SelmY4V-PC <2507917746@qq.com>
2023-01-26 16:45:02 +0800

    Ref 使用MayNum

--- lib/snt/xcr.d.ts
+++ lib/snt/xcr.d.ts
@@ -1,6 +1,6 @@
 import {
 	NumOfStr,
-	WideNum,
+	MayNum,
 } from '../..'
 import {
 	xcr,
@@ -11,10 +11,10 @@ type SntXcrUns<T extends 0 | 9, N extends string> = xcr.Ori<T, N>
 type SntXcr<T extends 0 | 9, N extends string> = `${T},${N}` extends '0,-1' ? '0' : `${T},${N}` extends '9,0' ? '-1' :
 	N extends `-${infer K}` ? `-${SntXcrUns<xcr.R<T>, K>}` : SntXcrUns<T, N>
 
-type SntXcrNum<T extends 0 | 9, N extends WideNum, K extends WideNum> = NumOfStr<SntXcr<T, `${N}`>, K>
+type SntXcrNum<T extends 0 | 9, N extends MayNum, K extends MayNum> = NumOfStr<SntXcr<T, `${N}`>, K>
 
-type Increase<N extends WideNum> = SntXcrNum<0, N, N extends number ? number : bigint>
-declare function increase<N extends WideNum>(num: N): Increase<N>
+type Increase<N extends MayNum> = SntXcrNum<0, N, N extends number ? number : bigint>
+declare function increase<N extends MayNum>(num: N): Increase<N>
 
-type Decrease<N extends WideNum> = SntXcrNum<9, N, N extends number ? number : bigint>
-declare function decrease<N extends WideNum>(num: N): Decrease<N>
+type Decrease<N extends MayNum> = SntXcrNum<9, N, N extends number ? number : bigint>
+declare function decrease<N extends MayNum>(num: N): Decrease<N>
