aaaf35e0c809b057eab47e6df1ad45633818d5a5
E0SelmY4V-PC <2507917746@qq.com>
2023-02-12 13:25:52 +0800

    Env 取消跟踪测试文件

--- test/testt.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var __1 = require("../");
-var a = 1;
-console.log((0, __1.removeLeading0)('00102420'));
--- test/testt.ts
+++
@@ -1,5 +0,0 @@
-import { SigNumber, removeLeading0, concat } from '../';
-let a: SigNumber = 1;
-console.log(removeLeading0('00102420'));
-const b = [1,2,3] as const
-const c = ['asd'] as const
