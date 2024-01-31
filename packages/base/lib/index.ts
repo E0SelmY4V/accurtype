34d6a2a9eeb3f001fbb7c6d3454f000f006ff18c
E0SelmY4V <yabo1887415157@qq.com>
2024-01-31 13:37:09 +0800

    Env base, calc 构建基本包和运算包的目录

---
+++ packages/base/lib/index.ts
@@ -0,0 +1,9 @@
+/**
+ * 精确类型～基本类型包
+ * @module \@accurtype/base
+ * @version 0.0.1
+ * @license MIT
+ */
+declare module '.';
+
+export { };
---
+++ packages/calc/lib/index.ts
@@ -0,0 +1,9 @@
+/**
+ * 精确类型～运算相关包
+ * @module \@accurtype/calc
+ * @version 0.0.1
+ * @license MIT
+ */
+declare module '.';
+
+export { };
