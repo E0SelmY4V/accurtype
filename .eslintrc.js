04b07a6e5f673ef055098bd908487dfac406b0cf
E0SelmY4V <yabo1887415157@qq.com>
2024-02-10 03:49:19 +0800

    Env 添加10个风格静态检查

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 /* eslint-env node */
+// @ts-check
 /**@type {import('eslint').ESLint.ConfigData} */
 module.exports = {
 	extends: [
@@ -10,7 +11,20 @@ module.exports = {
 	plugins: [
 		'@typescript-eslint',
 		'eslint-plugin-expect-type',
+		'@stylistic',
 	],
+	rules: {
+		'@stylistic/array-bracket-newline': ['error', 'consistent'],
+		'@stylistic/array-bracket-spacing': ['error', 'never'],
+		'@stylistic/array-element-newline': ['error', 'consistent'],
+		'@stylistic/arrow-parens': ['error', 'as-needed'],
+		'@stylistic/arrow-spacing': 'error',
+		'@stylistic/block-spacing': "error",
+		'@stylistic/brace-style': ["warn", "1tbs", { "allowSingleLine": true }],
+		'@stylistic/comma-dangle': ["error", "always-multiline"],
+		'@stylistic/comma-spacing': ["error", { "before": false, "after": true }],
+		'@stylistic/comma-style': ["error", "last"],
+	},
 	root: true,
 	parserOptions: {
 		project: ['./tsconfig.json', './packages/*/tsconfig.json'],
